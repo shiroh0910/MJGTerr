5440ba2917afcb8b3271bbc3308458030ee8ab57
Shiroh Okishima <s.okishima@gmail.com>
2025-10-06 00:12:54 +0900

    killsw

---
+++ public/kill-sw.js
@@ -0,0 +1,4 @@
+// This is an empty file to neutralize the old service worker.
+// このファイルは古いサービスワーカーを無力化するための空ファイルです。
+// This is an empty file to neutralize the old service worker.
+// このファイルは古いサービスワーカーを無力化するための空ファイルです。
