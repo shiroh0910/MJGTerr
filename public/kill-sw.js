ae3cacf3ac952c462f0b004cecab9e93f3a439aa
Shiroh Okishima <s.okishima@gmail.com>
2025-10-06 00:22:57 +0900

    レスキュー

--- public/kill-sw.js
+++
@@ -1,4 +0,0 @@
-// This is an empty file to neutralize the old service worker.
-// このファイルは古いサービスワーカーを無力化するための空ファイルです。
-// This is an empty file to neutralize the old service worker.
-// このファイルは古いサービスワーカーを無力化するための空ファイルです。
