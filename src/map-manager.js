6140082565de11180b6164abb5430417b741bb17
Shiroh Okishima <s.okishima@gmail.com>
2025-10-11 09:28:58 +0900

    不要なログを削除

--- src/map-manager.js
+++ src/map-manager.js
@@ -99,7 +99,6 @@ export class MapManager {
 
   async saveUserSettings(settings) {
     await this.userSettingsManager.save(settings);
-    console.log('User settings saved:', settings);
   }
 
   /**
